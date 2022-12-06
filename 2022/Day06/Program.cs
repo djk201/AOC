ea1cc24ad86e145f490367ee04e702d050809418
Dhaval Kapasi <dhaval.kapasi@mmc.com>
2022-12-06 12:19:08 +0530

    Day 6 solution

--- 2022/Day06/Program.cs
+++ 2022/Day06/Program.cs
@@ -7,8 +7,8 @@ namespace Day06
         {
             Console.WriteLine("Day 06!");
 
-            string inputFile = @"..\..\..\sample_input.txt";
-            //string inputFile = @"..\..\..\final_input.txt";
+            //string inputFile = @"..\..\..\sample_input.txt";
+            string inputFile = @"..\..\..\final_input.txt";
             var input = File.ReadAllLines(inputFile).ToList();
 
             Run(input);
@@ -19,21 +19,40 @@ namespace Day06
             var timer = new Stopwatch();
             timer.Start();
 
-            var answer1 = 0;
-            var answer2 = 0;
-
-            input.ForEach(x =>
-            {
-
-            });
-
+            int answer1 = FindMarker(input[0], 4);
             var answer1Time = timer.ElapsedMilliseconds;
+            Console.WriteLine($"Answer1 = {answer1}; Time Taken = {answer1Time} ms");
+
             timer.Restart();
 
+            var answer2 = FindMarker(input[0], 14);
             var answer2Time = timer.ElapsedMilliseconds;
-
-            Console.WriteLine($"Answer1 = {answer1}; Time Taken = {answer1Time} ms");
             Console.WriteLine($"Answer2 = {answer2}; Time Taken = {answer2Time} ms");
+        }
+
+        private static int FindMarker(string line, int queueSize)
+        {
+            var answer1 = 0;
+            var queue = new Queue<char>();
+            for (var i = 0; i < line.Length; i++)
+            {
+                if (queue.Contains(line[i]))
+                {
+                    char dequeuedItem = ' ';
+                    while (dequeuedItem != line[i])
+                    {
+                        dequeuedItem = queue.Dequeue();
+                    }
+                }
+                else if (queue.Count == queueSize-1)
+                {
+                    answer1 = i + 1;
+                    break;
+                }
+                queue.Enqueue(line[i]);
+            }
+
+            return answer1;
         }
     }
 }
