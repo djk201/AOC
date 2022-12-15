66ce31ff7d8d358ed73674e0c787af91644dcad6
Dhaval Kapasi <dhaval.kapasi@mmc.com>
2022-12-15 20:19:59 +0530

    part 2 draft

--- 2022/Day15/Program.cs
+++ 2022/Day15/Program.cs
@@ -9,14 +9,19 @@ namespace Day15
 
             //string inputFile = @"..\..\..\sample_input.txt";
             //int y = 10;
+            //int minCord = 0;
+            //int maxCord = 20;
             string inputFile = @"..\..\..\final_input.txt";
             int y = 2000000;
+            int minCord = 0;
+            int maxCord = 4000000;
+
             var input = File.ReadAllLines(inputFile).ToList();
 
-            Run(input, y);
+            Run(input, y, minCord, maxCord);
         }
 
-        private static void Run(List<string> input, int y)
+        private static void Run(List<string> input, int y, int minCord, int maxCord)
         {
             var timer = new Stopwatch();
             timer.Start();
@@ -26,9 +31,68 @@ namespace Day15
             Console.WriteLine($"Answer1 = {answer1}; Time Taken = {answer1Time} ms");
 
             timer.Restart();
-            var answer2 = 0;
+            var answer2 = GetTuningFrequencyForDistressBeacon(input, minCord, maxCord);
             var answer2Time = timer.ElapsedMilliseconds;
             Console.WriteLine($"Answer2 = {answer2}; Time Taken = {answer2Time} ms");
+        }
+
+        static int GetTuningFrequencyForDistressBeacon(List<string> input, int minCord, int maxCord)
+        {
+            List<Sensor> sensors = GetSensorAndRespectiveBeaconLocations(input);
+            Console.WriteLine("Initilized Sensors");
+            (int, int) distressBeaconPos = GetDistressBeaconPosition(sensors, minCord, maxCord);
+            return distressBeaconPos.Item1 * 4000000 + distressBeaconPos.Item2;
+        }
+
+        private static (int, int) GetDistressBeaconPosition(List<Sensor> sensors, int minCord, int maxCord)
+        {
+            //Dictionary<(int, int), bool> emptyLocations = new Dictionary<(int, int), bool>();
+
+            //sensors.ForEach(s =>
+            //{
+            //    for (int x = s.SensorPos.Item1 - s.DistanceToBeacon; x <= s.SensorPos.Item1 + s.DistanceToBeacon; x++)
+            //    {
+            //        var rangeY = s.DistanceToBeacon - Math.Abs(s.SensorPos.Item1 - x);
+            //        for (int y = s.SensorPos.Item2 - rangeY; y <= s.SensorPos.Item2 + rangeY; y++)
+            //        {
+            //            emptyLocations[new(x, y)] = true;
+            //        }
+            //    }
+            //});
+
+            bool isPointInSensorRange = false; ;
+            (int, int) distressBeacon = new (0,0);
+            for(int x = minCord; x <= maxCord; x++)
+            {
+                for (int y = minCord; y <= maxCord; y++)
+                {
+                    isPointInSensorRange = false;
+                    foreach (var s in sensors)
+                    {
+                        if (IsPointWithinSensorRange(s, x, y))
+                        {
+                            isPointInSensorRange = true;
+                            break;
+                        }
+                    };
+                    if (isPointInSensorRange) continue;
+                    distressBeacon = new (x, y);
+                    break;
+                }
+                if (!isPointInSensorRange) break;
+            }
+
+            Console.WriteLine("Identified DistressBeacon");
+
+            //int beaconX = Enumerable.Range(minCord, maxCord).Except(emptyLocations.Keys.Select(c => c.Item1)).FirstOrDefault();
+            //int beaconY = Enumerable.Range(minCord, maxCord).Except(emptyLocations.Keys.Select(c => c.Item2)).FirstOrDefault();
+            return distressBeacon;
+        }
+
+        static bool IsPointWithinSensorRange(Sensor sensor, int pointX, int pointY)
+        {
+            var distanceToPoint = Math.Abs(sensor.SensorPos.Item1 - pointX) + Math.Abs(sensor.SensorPos.Item2 - pointY);
+            return distanceToPoint <= sensor.DistanceToBeacon;
         }
 
         static int GetConfirmedEmptyLocationsForY(List<string> input, int y)
